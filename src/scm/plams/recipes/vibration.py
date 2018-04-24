7689a216db1dc04f8703e5b56d2b3847166ef470
Patrick Melix <patrick.melix@uni-leipzig.de>
2018-04-24 14:08:04 +0200

    rework of the vibration classes to fit the general PLAMS-scheme better

--- src/scm/plams/recipes/vibration.py
+++ src/scm/plams/recipes/vibration.py
@@ -18,20 +18,31 @@ except ImportError:
 
 
 class VibrationsResults(Results):
-    """A Class for handling numerical Vibrational analysis Results"""
-    pass
+    """
+    A Class for handling numerical Vibrational analysis Results.
+
+    Use ``get_ASEVib()`` to access the ``ase.vibrations.Vibrations`` object. Using e.g. ``self.get_ASEVib().summary()`` the results of the ASE frequency calculation are accessible.
+    """
+    def get_ASEVib(self):
+        """Returns a ``ase.vibrations.Vibrations`` object"""
+        return self._vib
+
 
 class VibrationsJob(MultiJob):
     """
     Class for calculating numerical Frequencies in parallel using arbitrary interfaces.
     This is achieved using the Vibrations class from ASE.
 
-    *   ``molecule`` -- Molecule (most propably in the chosen Methods optimized state)
+    *   ``molecule`` -- |Molecule| object (most propably in the chosen Methods optimized state)
     *   ``settings`` -- |Settings| instance for all Single-Point jobs to be run. Don't forget reference to a restart file if you want to save a lot of computer time!
-    *   ``jobType`` -- |Job| Class you want to use
-    *   ``get_gradients`` -- Function name to retrieve gradients of the |Result| object of your chosen jobType.results. Must take options eUnit='eV' and lUnit='Angstrom'
-    *   ``reorder`` -- Function name of jobType.results to reorder gradients to input order, set to ``None`` if not applicable.
-    *   ``aseVibOpt`` -- Options for ase.vibrations.Vibrations.__init__
+    *   ``jobType`` -- |Job| Class you want to use.
+    *   ``get_gradients`` -- Function name to retrieve gradients of the |Results| object of your chosen ``jobType.results``. Must take options ``eUnit='eV'`` and ``lUnit='Angstrom'``.
+    *   ``reorder`` -- Function name of ``jobType.results`` to reorder gradients to input order, set to ``None`` if not applicable.
+    *   ``aseVibOpt`` -- Options for ``ase.vibrations.Vibrations.__init__``
+
+    The ``self.__init__()`` method calls on ase.vibrations.Vibrations.run to create all displacements, then adds them as children.
+    After running, the ``self.postrun()`` method reads the gradients from all single-points and saves them according to the ASE scheme.
+    Finally the ASE Vibrations object is made accessible in ``self.results.get_ASEVib`` for the retrieval of the ASE results.
 
     """
     _result_type = VibrationsResults
@@ -51,15 +62,23 @@ class VibrationsJob(MultiJob):
         else:
             self.reorder = lambda x: x
 
-    def prerun(self):
+
+    def new_children(self):
         #Create displaced molecules with ASE
         #Include our workingdir in the name, this results in the ASE .pckl files to be saved there
         #kinda hacky but works
-        path = self.path
-        self.vib = aseVib(toASE(self.molecule), name=osPJ(path,'plams.vib'), **self.aseVibOpt)
-        aseMols = self.vib.run(export='return')
-        for key, item in aseMols.items():
-            self.children.append(self.jobType(molecule=fromASE(item), name=osPB(key).replace('.pckl',''), settings=self.settings))
+
+        if len(self.children) == 0:
+            add = []
+            path = self.path
+            self._vib = aseVib(toASE(self.molecule), name=osPJ(path,'plams.vib'), **self.aseVibOpt)
+            aseMols = self._vib.run(export='return')
+            for key, item in aseMols.items():
+                add.append(self.jobType(molecule=fromASE(item), name=osPB(key).replace('.pckl',''), settings=self.settings))
+            return add
+
+        else:
+            return None
 
 
     def postrun(self):
@@ -68,9 +87,11 @@ class VibrationsJob(MultiJob):
         for child in self.children:
             res = child.results
             name = child.name
-            force = -1.0 * npa(self.reorder(res, self.get_grad(res, eUnit='eV', lUnit='Angstrom')))
+            # don't rely on gradients beeing numpy arrays
+            f = [ npa(vec) for vec in self.get_grad(res, eUnit='eV', lUnit='Angstrom') ]
+            force = -1.0 * npa(self.reorder(res, f))
             filename = osPJ(path, name+'.pckl')
             with open(filename, 'wb') as f:
                 pickleDump(force, f, protocol=2)
 
-        self.results.vib = self.vib
+        self.results._vib = self._vib
