4ba9ed678ece76f14916290950c62d4cb0c0fa96
Michal Handzlik <handzlik@scm.com>
2019-03-18 16:43:32 +0100

    Add dihedral() function to geometry module

--- tools/geometry.py
+++ tools/geometry.py
@@ -7,7 +7,7 @@ except ImportError:
 
 from .units import Units
 
-__all__ = ['rotation_matrix', 'axis_rotation_matrix', 'distance_array']
+__all__ = ['rotation_matrix', 'axis_rotation_matrix', 'distance_array', 'dihedral']
 
 def rotation_matrix(vec1, vec2):
     """
@@ -48,3 +48,26 @@ def distance_array(array1, array2):
     Uses fast ``cdist`` function if ``scipy`` is present, otherwise falls back to slightly slower ``numpy`` loop. Arguments should be 2-dimensional ``numpy`` arrays with the same second dimension. If *array1* is A x N and *array2* is B x N, the returned array is A x B.
     """
     return cdist(array1, array2) if scipy_present else np.array([np.linalg.norm(i - array2, axis=1) for i in array1])
+
+
+
+def dihedral(p1, p2, p3, p4, unit='radian'):
+    """Calculate the value of diherdal angle formed by points *p1*, *p2*, *p3* and *p4* in a 3D space. Arguments can be any containers with 3 numerical values, also instances of |Atom|. Returned value is always non-negative, measures the angle clockwise (looking along *p2-p3* vector) and is expressed in *unit*."""
+    p1 = np.array([*p1], dtype=float)
+    p2 = np.array([*p2], dtype=float)
+    p3 = np.array([*p3], dtype=float)
+    p4 = np.array([*p4], dtype=float)
+
+    b0 = p1 - p2
+    b1 = p3 - p2
+    b2 = p4 - p3
+
+    b1 /= np.linalg.norm(b1)
+    v = b0 - np.dot(b0, b1)*b1
+    w = b2 - np.dot(b2, b1)*b1
+
+    x = np.dot(v, w)
+    y = np.dot(np.cross(b1, v), w)
+    ret = np.arctan2(y, x)
+    ret = 2*np.pi+ret if ret < 0 else ret
+    return Units.convert(ret, 'radian', unit)
