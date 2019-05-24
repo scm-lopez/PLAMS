0b96b1a9c7603c4a6190f9773458890979e241b9
Bas van Beek <b.f.van.beek@vu.nl>
2019-05-24 10:47:39 +0200

    Revert "Grant the Cp2kResults.get_energy() and Cp2kResults.get_dispersion() access to the 'unit' keyword"
    This reverts commit cfde76fbec2da8467d798ab54be6bba990e99339.

--- interfaces/thirdparty/cp2k.py
+++ interfaces/thirdparty/cp2k.py
@@ -8,7 +8,6 @@ from ...core.results import Results
 from ...core.errors import ResultsError
 from ...mol.molecule import Molecule
 from ...mol.atom import Atom
-from ...tools.units import Units
 
 __all__ = ['Cp2kJob', 'Cp2kResults', 'Cp2kSettings2Mol']
 
@@ -146,15 +145,13 @@ class Cp2kResults(Results):
         s = self.grep_output(search+' energy:')[select].split()[-1]
         return float(s)
 
-    def get_energy(self, index=0, unit='au'):
+    def get_energy(self, index=0):
         """Returns last occurence of 'Total energy:' in the output."""
-        ret = self._get_energy_type('Total', index=index)
-        return Units.convert(ret, 'au', unit)
+        return self._get_energy_type('Total', index=index)
 
-    def get_dispersion(self, index=0, unit='au'):
+    def get_dispersion(self, index=0):
         """Returns last occurence of 'Dispersion energy:' in the output."""
-        ret = self._get_energy_type('Dispersion', index=index)
-        return Units.convert(ret, 'au', unit)
+        return self._get_energy_type('Dispersion', index=index)
 
     def _idx_to_match(self, nTotal, idx):
         if idx is None:
