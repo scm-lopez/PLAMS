cfb246bd5acbb7dd7ea57b9f77c1d1e040f248c0
scmtestadf <vanschoot@scm.com>
2020-06-17 17:15:38 +0200

    Automatic Sync from SCM-svn to GitHub syncing the following commits: MirkoFranchini: Two pull requests to PLAMS from Bas were accidentally reverted by the svn auto commit. Here I reintroduce the two pull requests (pull requests number 86 and 81) SO-- SCMSUITE-5508

--- unit_tests/test_molecule.py
+++ unit_tests/test_molecule.py
@@ -1,7 +1,14 @@
+import os
 from pathlib import Path
 
 import numpy as np
+try:
+    import dill as pickle
+except ImportError:
+    import pickle
+
 from scm.plams import Molecule, Atom, MoleculeError
+
 
 PATH = Path('unit_tests') / 'xyz'
 BENZENE = Molecule(PATH / 'benzene.xyz')
@@ -77,3 +84,62 @@ def test_round_coords():
 
     benzene.round_coords(decimals=2)
     np.testing.assert_allclose(benzene, ref2)
+
+
+IMMUTABLE_TYPE = (int, float, tuple)
+ATTR_EXCLUDE = frozenset({'mol', 'bonds', 'atoms', 'atom1', 'atom2'})
+
+
+def _compare_attrs(obj1, obj2, eval_eq=True):
+    assert obj1 is not obj2
+
+    for name, attr in vars(obj1).items():
+        if name in ATTR_EXCLUDE:
+            continue
+
+        attr_ref = getattr(obj2, name)
+        if eval_eq:
+            assert attr == attr_ref
+        if not isinstance(attr, IMMUTABLE_TYPE):
+            assert attr is not attr_ref
+
+
+def test_copy(mol_list=None):
+    """Tests for :meth:`Molecule.copy`, :meth:`Molecule.__copy__` and :meth:`Molecule.__deepcopy__`."""
+    if mol_list is None:
+        mol_list = [
+            BENZENE.copy(),
+            BENZENE.__copy__(),
+            BENZENE.__deepcopy__({})
+        ]
+
+    for mol in mol_list:
+        _compare_attrs(mol, BENZENE, eval_eq=False)
+
+        for at, at_ref in zip(mol.atoms, BENZENE.atoms):
+            _compare_attrs(at, at_ref)
+
+        for bond, bond_ref in zip(mol.bonds, BENZENE.bonds):
+            _compare_attrs(bond, bond_ref)
+
+
+def test_set_get_state():
+    """Tests for :meth:`Molecule.__setstate__` and :meth:`Molecule.__getstate__`."""
+    mol = BENZENE.copy()
+    dill_new = PATH / 'benzene_new.dill'
+
+    # An old pickle file created before the introduction of Molecule.__getstate__
+    dill_old = PATH / 'benzene_old.dill'
+
+    try:
+        with open(dill_new, 'wb') as f:
+            pickle.dump(mol, f)
+        with open(dill_new, 'rb') as f:
+            mol_new = pickle.load(f)
+
+        with open(dill_old, 'rb') as f:
+            mol_old = pickle.load(f)
+
+        test_copy(mol_list=[mol_new, mol_old])
+    finally:
+        os.remove(dill_new) if os.path.isfile(dill_new) else None
