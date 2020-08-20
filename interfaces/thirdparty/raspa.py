a487946ec36a2608037edbe4e549e10203271100
Patrick Melix <patrick.melix@uni-leipzig.de>
2020-08-20 10:48:23 +0200

    raspa results functions

--- interfaces/thirdparty/raspa.py
+++ interfaces/thirdparty/raspa.py
@@ -11,7 +11,7 @@ from os import symlink
 from ...core.basejob import SingleJob
 from ...core.settings import Settings
 from ...core.results import Results
-from ...tools.units import Units
+from ...core.errors import ResultsError, JobError
 
 __all__ = ['RaspaJob', 'RaspaResults']
 
@@ -34,9 +34,180 @@ class RaspaResults(Results):
             if files:
                 self.job._filenames['out'] = files[0]
 
+    def get_block_value(self, search, file=None, get_std=False, get_unit=False):
+        """Return a Block Value from Output File.
 
+        Can be used to retrieve block values from the final section of the output files.
+        Uses `Results.grep_file` to retrieve the values.
+        Remember to escape special `grep` characters in the search string.
+        Returns the average value over all blocks.
 
+        search: string
+        String labeling the value you desire.
 
+        file: string
+        Defaults to the automatically detected output file. See `self.collect()`.
+
+        get_std: boolean
+        Also return the standard deviation of the value
+
+        get_unit: boolean
+        Also return the unit of the value
+        """
+        if not file:
+            file = self.job._filenames['out']
+
+        chunk = self.grep_file(file, pattern=search, options='-A 8')
+        last_line = chunk[-1].strip()
+        if not last_line.startswith('Average'):
+            raise ResultsError(
+                "Unable to get the requested value from {:}".format(file))
+
+        last_line = last_line.split()
+        ret = [float(last_line[1])]
+
+        if get_std:
+            ret.append(float(last_line[4]))
+        if get_unit:
+            ret.append(last_line[-1][1:-1])
+
+        if len(ret) > 1:
+            return tuple(ret)
+        else:
+            return ret[0]
+
+    def get_value(self, search, file=None, get_std=False, get_unit=False):
+        """Return a Single Value from Output File.
+
+        Can be used to retrieve single values from the final section of the output files.
+        Uses `Results.grep_file` to retrieve the values.
+        Remember to escape special `grep` characters in the search string.
+
+        search: string
+        String labeling the value you desire.
+
+        file: string
+        Defaults to the automatically detected output file. See `self.collect()`.
+
+        get_std: boolean
+        Also return the standard deviation of the value
+
+        get_unit: boolean
+        Also return the unit of the value
+        """
+        if not file:
+            file = self.job._filenames['out']
+
+        line = self.grep_file(file, pattern=search)[-1].split()
+        # Two cases, with standard deviation and without
+        if '+/-' in line:
+            idx = line.index('+/-') - 1
+            ret = [float(line[idx])]
+            if get_std:
+                ret.append(float(line[idx + 2]))
+        else:
+            if get_std:
+                raise ResultsError(
+                    "Did not find +/- in {:}. Does the value you requested have one?!".format(file))
+            # extract first float
+            for s in line:
+                try:
+                    ret = [float(s)]
+                    break
+                except BaseException:
+                    continue
+        # unit always in [], sometimes at the end, sometimes in the middle.
+        # always use the first [] to find the unit (sometimes it is given twice
+        # and the second one is empty)
+        if get_unit:
+            line = " ".join(line)
+            idx1 = line.index('[')
+            idx2 = line.index(']')
+            ret.append(line[idx1 + 1:idx2])
+
+        if len(ret) > 1:
+            return tuple(ret)
+        else:
+            return ret[0]
+
+    def get_from_all_files(self, *args, output_folder='Output/System_0/', method='get_value',**kwargs):
+        """Wrapper to Execute Methods on all Output files.
+
+        output_folder: string
+        The subfolder containing the relevant output files. Default should be fine.
+
+        method: function
+        The function to call, needs to be a function of the |RaspaResults| class that takes a *file* argument.
+        All *args* and *kwargs* are passed on.
+
+        If the *method* returns tuples, the tuples are unpacked and added to lists.
+        """
+        call_method = getattr(self, method)
+        relevant_files = [f for f in self.files if output_folder in f]
+        ret = []
+        for f in relevant_files:
+            data = call_method(*args, file=f, **kwargs)
+            if isinstance(data, tuple):
+                # initialize ret list to proper length
+                if len(ret) == 0:
+                    ret = [[] for i in range(len(data))]
+                for i, d in enumerate(data):
+                    ret[i].append(d)
+            else:
+                ret.append(data)
+
+        if isinstance(ret[0], list):
+            return tuple(ret)
+        else:
+            return ret
+
+    def get_isotherm(self, output_folder='Output/System_0/',
+            search_x='Partial pressure:',
+            search_y=r'Average loading excess \[cm^3 (STP)/gr',
+            get_std=False, get_unit=False):
+        """Try to automatically collect Isotherm Data.
+
+        output_folder: string
+        The subfolder containing the relevant output files. Default should be fine.
+
+        search_x: string
+        String labeling the desired x-Axis values in the output.
+
+        search_y: string
+        String labeling the desired y-Axis values in the output.
+
+        get_std: boolean
+        Return the standard deviation of the y values as a list.
+
+        get_unit: boolean
+        Return the units of the values as strings `x_unit`, `y_unit`.
+
+        Returns two lists: `x_values`, `y_values`. Optionally after that `x_std`, `x_unit`, `y_unit`.
+        """
+        ret = []
+        x_info = self.get_from_all_files(
+            search_x, output_folder=output_folder, get_unit=get_unit)
+        y_info = self.get_from_all_files(
+            search_y,
+            output_folder=output_folder,
+            get_std=get_std,
+            get_unit=get_unit)
+
+        if not get_std and not get_unit:
+            ret = [x_info, y_info]
+        elif get_std and not get_unit:
+            ret = [x_info, *y_info]
+        elif get_std:
+            ret = [x_info[0], *y_info[0:2], x_info[1]
+                   [0], y_info[2][0]]  # only one unit
+        else:
+            ret = [
+                x_info[0],
+                y_info[0],
+                x_info[1][0],
+                y_info[1][0]]  # only one unit
+
+        return tuple(ret)
 
 
 class RaspaJob(SingleJob):
@@ -55,15 +226,18 @@ class RaspaJob(SingleJob):
     The environment variable ``$RASPA_DIR`` needs to be set for the runscript to work.
     """
     _result_type = RaspaResults
-    _filenames = {'inp':'simulation.input', 'run':'$JN.run', 'err': '$JN.err', 'stdout': '$JN.out'}
-
+    _filenames = {
+        'inp': 'simulation.input',
+        'run': '$JN.run',
+        'err': '$JN.err',
+        'stdout': '$JN.out'}
 
     def __init__(self, copy=None, symlink=None, **kwargs):
         SingleJob.__init__(self, **kwargs)
         self.copy_files = copy
         self.symlink_files = symlink
-        self.settings.runscript.stdout_redirect = True #Output Name is not known before running
-
+        # Output Name is not known before running
+        self.settings.runscript.stdout_redirect = True
 
     def _get_ready(self):
         """Copy files to execution dir if self.copy_files is set."""
@@ -78,7 +252,6 @@ class RaspaJob(SingleJob):
                 symlink(path, dest)
         return
 
-
     def get_input(self):
         """Transform all contents of ``settings.input`` branch into string with blocks, keys and values.
 
@@ -92,7 +265,7 @@ class RaspaJob(SingleJob):
         """
 
         def parse(key, value, indent=''):
-            #transform bools to yes and no
+            # transform bools to yes and no
             if value is True:
                 value = 'yes'
             elif value is False:
@@ -103,23 +276,25 @@ class RaspaJob(SingleJob):
             if (key.lower() == 'component'):
                 n_comp = len(value)
                 for i in range(n_comp):
-                    #get component number
+                    # get component number
                     if str(i) in value:
                         number = str(i)
                     elif "_{}".format(i) in value:
                         number = "_{}".format(i)
                     else:
-                        raise PlamsError("Check that you have a valid component {:} in settings.input.component".format(i))
+                        raise JobError(
+                            "Check that you have a valid component {:} in settings.input.component".format(i))
                     ret += ' ' + str(i)
 
                     subkeys = list(value[number].keys())
-                    mol_name = subkeys[[sk.lower() for sk in subkeys].index('moleculename')]
+                    mol_name = subkeys[[sk.lower()
+                                        for sk in subkeys].index('moleculename')]
                     ret += ' MoleculeName ' + str(value[number][mol_name])
                     ret += '\n'
                     tmp_value = value[number].copy()
-                    del tmp_value[mol_name]#exclude MoleculeName
+                    del tmp_value[mol_name]  # exclude MoleculeName
                     for el in tmp_value:
-                        ret += parse(el, tmp_value[el], indent+'  ')
+                        ret += parse(el, tmp_value[el], indent + '  ')
                     ret += '\n' + indent + key
 
                 return ret
@@ -131,7 +306,7 @@ class RaspaJob(SingleJob):
 
                 for el in value:
                     if el != '_h':
-                        ret += parse(el, value[el], indent+'  ')
+                        ret += parse(el, value[el], indent + '  ')
             else:
                 ret += ' ' + str(value)
             ret += '\n'
@@ -143,7 +318,6 @@ class RaspaJob(SingleJob):
 
         return inp
 
-
     def get_runscript(self):
         """$RASPA_DIR has to be set in the environment!"""
         ret = 'export DYLD_LIBRARY_PATH=${RASPA_DIR}/lib'
@@ -153,7 +327,6 @@ class RaspaJob(SingleJob):
             ret += ' >' + self._filename('stdout')
         ret += '\n\n'
         return ret
-
 
     def check(self):
         """Look for the normal termination signal in output."""
