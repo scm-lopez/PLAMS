958a8a3e7dbf074ebf0e3255f9b006188b4f6838
scmtestadf <vanschoot@scm.com>
2021-03-16 18:20:41 +0100

    Automatic Sync from SCM-svn to GitHub syncing the following commits: RosaBulo: Rosa Bulo (REB) SCMSUITE-647 SO102
    Imerged the analysis tool refactoring from this branch:
    branches/RosaBulo/trajectory_analysis
    into the trunk.
    Mainly it makes the analysis tool more modular, so that it becomes easier to add a new property,
    particularly to the ACF analysis.

--- interfaces/adfsuite/amsanalysis.py
+++ interfaces/adfsuite/amsanalysis.py
@@ -1,7 +1,7 @@
 from ...core.errors import PlamsError
 from .scmjob import SCMJob, SCMResults
 
-__all__ = ['AMSAnalysisJob', 'AMSAnalysisResults']
+__all__ = ['AMSAnalysisJob', 'AMSAnalysisResults','convert_to_unicode']
 
 class AMSAnalysisXYResults:
     def __init__(self):
@@ -9,10 +9,32 @@ class AMSAnalysisXYResults:
         self.y = []
         self.x_units = None
         self.y_units = None
+        self.x_name = None
+        self.y_name = None
         self.y_sigma = [] # stadard deviation for y_values
 
         self.properties = None
         self.legend = None
+
+    def read_data (self, kf, sec) :
+        """
+        Read the xy data for a section from the kf file
+        """
+        xkey = 'x(1)-axis'
+        x_name = kf.read(sec, '%s(label)'%(xkey))
+        self.x = kf.read(sec, xkey)
+        self.x_name = convert_to_unicode(x_name)
+        self.x_units = convert_to_unicode(kf.read(sec, '%s(units)'%(xkey)))
+
+        ykey = 'y-axis'
+        y_name = kf.read(sec, '%s(label)'%(ykey))
+        self.y = kf.read(sec, ykey)
+        self.y_name = convert_to_unicode(y_name)
+        self.y_units = convert_to_unicode(kf.read(sec, '%s(units)'%(ykey)))
+
+        self.y_sigma = kf.read(sec, 'sigma')
+
+        self.read_properties(kf, sec)
 
     def read_properties (self, kf, sec) :
         """
@@ -23,17 +45,43 @@ class AMSAnalysisXYResults:
         while(1) :
             counter += 1
             try :
-                propname = kf.read(sec, 'Property(%i)'%(counter))
+                propname = kf.read(sec, 'Property(%i)'%(counter)).strip()
             except :
                 break
             properties[propname] = kf.read(sec, propname)
+            if isinstance(properties[propname],str) :
+                properties[propname] = properties[propname].strip()
+
+        # Now set the instance variables
         self.properties = properties
         if 'Legend' in properties :
             self.legend = properties['Legend']
 
+    def write_plot (self, outfilename=None) :
+        """
+        Print this plot to a text file
+        """
+        parts = []
+        for propname,prop in self.properties.items() :
+            parts.append('%-30s %s\n'%(propname, prop))
+        x_name = '%s(%s)'%(self.x_name,self.x_units)
+        y_name = '%s(%s)'%(self.y_name,self.y_units)
+        parts.append('%30s %30s %30s\n'%(x_name,y_name,'sigma'))
+        for x,y,s in zip(self.x,self.y,self.y_sigma) :
+            parts.append('%30.10e %30.10e %30.10e\n'%(x,y,s))
+        block = ''.join(parts)
+
+        if outfilename is not None :
+            outfile = open(outfilename,'w',encoding='utf8')
+            outfile.write(block)
+            outfile.close()
+
+        return block
+        
+
 class AMSAnalysisResults(SCMResults):
     _kfext = '.kf'
-    _rename_map = {'analysis.kf':'$JN'+_kfext}
+    _rename_map = {'plot.kf':'$JN'+_kfext}
 
     def get_molecule(self, *args, **kwargs):
         raise PlamsError('AMSAnalysisResults does not support the get_molecule() method.')
@@ -44,6 +92,8 @@ class AMSAnalysisResults(SCMResults):
         """
         if not self._kfpresent():
             raise FileError('File {} not present in {}'.format(self.job.name+self.__class__._kfext, self.job.path))
+        if self._kf.reader._sections is None :
+            self._kf.reader._create_index()
         sections = self._kf.reader._sections.keys()
         return sections
 
@@ -56,33 +106,43 @@ class AMSAnalysisResults(SCMResults):
 
         # Find the correct section in the KF file
         sections = self.get_sections()
-        matches = [s for s in sections if s.lower()==section.lower()+'('+str(i)+')']
+        matches = [s for s in sections if s.lower()==section.lower()+'(%i)'%(i)]
         if len(matches) == 0 :
                 print ('Sections: ',list(sections))
                 raise PlamsError('AMSAnalysisResults.get_xy(section,i): section must be one of the above. You specified "{}"'.format(section))
         sec = matches[0] 
 
         # Get the data
-        x_name = self.readkf(sec, 'x(1)-axis')
-        xy.x = self.readkf(sec, x_name) 
-        xy.x_units = self.readkf(sec, '%s(units)'%(x_name))
-
-        y_name = self.readkf(sec, 'y-axis')
-        xy.y = self.readkf(sec, y_name) 
-        xy.y_units = self.readkf(sec, '%s(units)'%(y_name))
-
-        xy.y_sigma = self.readkf(sec, 'sigma')
-
-        xy.read_properties(self._kf, sec)
+        xy.read_data(self._kf,sec)
 
         return xy
+
+    def write_all_plots (self) :
+        """
+        Get a list of all the plots created by the analysis job
+        """
+        sections = self.get_sections()
+        for section in sections :
+            name_part = section.split('(')[0]
+            num_part = int(section.split('(')[1].split(')')[0])
+            outfilename = '%s_%i.dat'%(name_part,num_part)
+            xy = self.get_xy(name_part,num_part)
+            xy.write_plot('%s'%(outfilename))
 
     def get_D(self, i=1):
         """ returns a 2-tuple (D, D_units) from the AutoCorrelation(i) section on the .kf file. """
 
-        sec = 'AutoCorrelation('+str(i)+')'
-        D = self.readkf(sec, 'DiffusionCoefficient')
-        D_units = self.readkf(sec, 'DiffusionCoefficient(units)')
+        # If there are multiple, it will read the first one
+        sections = [sec for sec in self.get_sections() if 'Integral' in sec]
+        if len(sections) < i : 
+            return None,None
+        section = sections[i-1]
+        plot = self.get_xy(section.split('(')[0],i)
+        if not 'DiffusionCoefficient' in plot.properties.keys() :
+            return None, None
+
+        D = plot.properties['DiffusionCoefficient']
+        D_units = plot.y_units
         return D, D_units
 
 
@@ -93,6 +153,7 @@ class AMSAnalysisJob(SCMJob):
     """
     _result_type = AMSAnalysisResults
     _command = 'analysis'
+    _subblock_end = 'end'
 
     def __init__(self, **kwargs):
         SCMJob.__init__(self, **kwargs)
@@ -109,3 +170,18 @@ class AMSAnalysisJob(SCMJob):
         except:
             return False
         return len(grep) > 0
+
+def convert_to_unicode (k) :
+    """
+    Convert a string with ascii symbols representing unicode symbols
+
+    Example k: 'abc\\u03c9def'
+    """
+    parts = k.split('\\u')
+    # Collect the hexadecimals
+    symbols = [chr(int(part[:4],16)) for part in parts[1:]]
+    # Now repair the parts
+    parts = parts[:1] + [''.join([s,part[4:]]) for s,part in zip(symbols,parts[1:])]
+    key = ''.join(parts)
+
+    return key
