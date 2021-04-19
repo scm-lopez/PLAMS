295a4d4d3b57221f5e96f84ff66f6105d2a2d9b0
Patrick Melix <patrick.melix@uni-leipzig.de>
2021-04-19 14:31:39 +0200

    fix order mistake

--- recipes/vibration.py
+++ recipes/vibration.py
@@ -51,7 +51,7 @@ class VibrationsJob(MultiJob):
     """
     _result_type = VibrationsResults
 
-    def __init__(self, name='plams.vib', molecule, settings, jobType=ADFJob, get_gradients='get_gradients', reorder='inputOrder', aseVibOpt={}):
+    def __init__(self, molecule, settings, name='plams.vib', jobType=ADFJob, get_gradients='get_gradients', reorder='inputOrder', aseVibOpt={}):
         MultiJob.__init__(self, name=name)
         self.molecule = molecule
         self.settings = settings
